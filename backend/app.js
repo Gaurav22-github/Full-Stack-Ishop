457c587acba6ce2d6876e23c5787269b8df2f760
Gaurav22-github <rj14gaurav@gmail.com>
2025-05-15 11:05:40 +0530

    first-commit

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+require('dotenv').config(); // Load environment variables
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const CategoryRouter = require('./routers/category.router');
+const ProductRouter = require('./routers/product.router');
+const colorRouter = require('./routers/color.router');
+const UserRouter = require('./routers/user.router');
+const CartRouter = require('./routers/cart.router');
+const AdminRouter = require('./routers/admin.router');
+const OrderRouter = require('./routers/order.router');
+const TransactionRouter = require("./routers/transaction.router");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// hosting public folder in localhost
+app.use(express.static("public"))
+// Middleware
+app.use(express.json());
+// origin : '*' this option allows requests from any origin
+app.use(cors({
+    origin: '*',
+}));
+
+// MongoDB connection
+// localhost => 127.0.0.1
+mongoose.connect('mongodb://127.0.0.1:27017/', {
+    dbName: "wsjp61"
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err.message));
+
+// Routes
+app.use("/category", CategoryRouter);
+app.use("/product", ProductRouter);
+app.use("/color" , colorRouter)
+app.use("/user", UserRouter)
+app.use("/cart",CartRouter)
+app.use("/admin", AdminRouter)
+app.use("/order",OrderRouter)
+app.use("/transaction",TransactionRouter)
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
